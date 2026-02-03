6a161c11fde9062394f02455f7272f4b166dbf5b
Riccardo Zazza <riccardo.zazza@icloud.com>
2026-02-03 15:14:48 +0100

    Add additional test cases for Calculator class

--- src/test/java/com/example/app/CalculatorTest.java
+++ src/test/java/com/example/app/CalculatorTest.java
@@ -1,14 +1,20 @@
 package com.example.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
     @Test
-    void testAdd() {
+    void testAdd1() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3));
+    }
+
+     @Test
+    void testAdd2() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(8, 3));
     }
 
     @Test
